bc8578c0af3ca980ae8f790b82149e70bd8a0162
Quinton Dean <dean.quinton@ymail.com>
2023-12-18 16:55:48 -0600

    charges into export variable

--- player/gun.gd
+++ player/gun.gd
@@ -1,33 +1,24 @@
 class_name Gun extends Node2D
 
 @export var bullet_scene: PackedScene
+@export var max_charge: int = 10
 
 const SCALE_DEGREE = 5
 
-var max_charge: int = 10
 var weapon_charge: int
 
 signal refill_charge(int)
 
-# var weapon_resource: WeaponResource
-# var bullet_resource: BulletResource
-
 func _ready():
 	weapon_charge = max_charge
 	refill_charge.connect(_refill_weapon_charge)
-	# if weapon_resource:
-	# 	if weapon_resource.texture:
-	# 		$Sprite2D.texture = weapon_resource.texture
-	# 	if weapon_resource.bullet_scene:
-	# 		bullet_scene = weapon_resource.bullet_scene
-	# 	if weapon_resource.bullet_resource:
-	# 		bullet_resource = weapon_resource.bullet_resource
 
 func _physics_process(_delta):
 	orbit_around_parent()
 
 func orbit_around_parent():
 	var mouse_position: Vector2 = get_global_mouse_position()
+
 	# Get the vector between the player and the mouse position and normalize it
 	var direction: Vector2 = (mouse_position - get_parent().position).normalized() * SCALE_DEGREE
 	self.position = direction
@@ -37,15 +28,10 @@ func shoot():
 		weapon_charge -= 1
 		get_parent().debug_ui.update_ammo_text.emit(str(weapon_charge))
 		var bullet = bullet_scene.instantiate()
-		# bullet.resource = bullet_resource
 		bullet.position = self.global_position
 		bullet.direction = (get_global_mouse_position() - get_parent().global_position).normalized()
 		get_parent().call_deferred("add_sibling", bullet)
 	
-# func set_stats(weapon_stats: WeaponResource):
-# 	bullet_scene = weapon_stats.bullet_scene
-# 	$Sprite2D.texture = weapon_stats.texture
-#	  bullet_resource = weapon_stats.bullet_resource
 
 func _refill_weapon_charge(charge: int):
 	weapon_charge += charge
--- player/gun_bullet.gd
+++ player/gun_bullet.gd
@@ -4,33 +4,18 @@ class_name GunBullet extends Area2D
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var animation: AnimatedSprite2D = $AnimatedSprite2D
 
-# @export var resource: BulletResource
-
 @export var SPEED = 400
 @export var KNOCKBACK_POWER = 100.0
 @export var attack_damage = 1
 var direction = Vector2.RIGHT
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	animation.play("default")
-	# if resource:
-	# 	SPEED = resource.bullet_speed
-	# 	KNOCKBACK_POWER = resource.knockback_power
-	# 	$Sprite2D.texture = resource.texture 
 
 	visible_on_screen.screen_exited.connect(_free_me)
-	# self.body_entered.connect(_hit_register)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position += direction * SPEED * delta
-
-# func _hit_register(body: Node2D):
-# 	if body is Enemy:
-# 		var knockback_direction := Vector2(direction.x * KNOCKBACK_POWER, direction.y * KNOCKBACK_POWER)
-# 		body.take_damage(knockback_direction)
-# 		self.queue_free()
 
 func _free_me():
 	self.queue_free()
