21868dfb0971d5ef5ce62aa91a335ef8a933448a
Quinton Dean <dean.quinton@ymail.com>
2024-01-02 08:57:38 -0600

    add hurtbox and health ui

--- enemies/warden/warden.gd
+++ enemies/warden/warden.gd
@@ -7,7 +7,10 @@ extends CharacterBody2D
 
 @onready var weapon_component: WeaponComponent = $weapon_component
 
+@onready var health: HealthComponent = $health_component
+@onready var hurtbox: Hurtbox = $Hurtbox
 @onready var player_detection: Area2D = $player_detection
+@onready var health_bar: Control = $health_bar
 
 @export var player_node_name: String
 
@@ -32,8 +35,8 @@ func _ready():
 
 	movement_state_machine.init(self, animated_sprite, movement_component)
 	attack_state_machine.init(self, animated_sprite, movement_component)
-	# hurtbox.take_damage.connect(_take_damage_bus)
-	# health.dead.connect(_handle_death)
+	hurtbox.take_damage.connect(_take_damage_bus)
+	health.dead.connect(_handle_death)
 
 func _unhandled_input(event):
 	movement_state_machine.process_input(event)
@@ -45,3 +48,23 @@ func _physics_process(delta):
 func _process(delta):
 	movement_state_machine.process_frame(delta)
 	attack_state_machine.process_frame(delta)
+
+func _take_damage_bus(damage: int):
+	health.damaged.emit(damage)
+	var percentage = health.get_percentage_health()
+	health_bar.update_ui_percentage.emit(percentage)
+	for x in 4:
+		animated_sprite.self_modulate.a = 0.5
+		await get_tree().process_frame
+		await get_tree().process_frame
+		animated_sprite.self_modulate.a = 1.0
+		await get_tree().process_frame
+		await get_tree().process_frame
+
+func _handle_death():
+	hurtbox.take_damage.disconnect(_take_damage_bus)
+	health.dead.disconnect(_handle_death)
+
+	# set the death state that will take care of the queue_free()
+	self.queue_free()
+	pass
